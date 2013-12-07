ea56cf5727d220f190165880b12a0793cce12f15
Theres Scherrer <theresscherrer@gmail.com>
2013-12-07 01:45:09 -0800

    Page to explain how to upload pictures

---
+++ upload.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	include("Database.php");
+    require('header.php');
+
+    // $person = Person::currentUser();
+
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $person->becomeFriendsWith($_POST['action']);
+      exit();
+    }
+?>
+
+    <div class="container">
+      <?php
+        flash();
+      ?>
+
+    </div>
+</body>
+</html>
