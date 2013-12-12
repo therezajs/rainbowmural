b5cc82eeffb0b022b09c9c4d220f4f4389fb2e75
Theres Scherrer <theresscherrer@gmail.com>
2013-12-12 00:08:32 -0800

    Added user information and log off button to the page

--- edit.php
+++ edit.php
@@ -6,8 +6,21 @@
 
 ?>
 
-    <div class="container">
+    <div class="container" id="my_container">
       <?php
         flash();
       ?>
+
+	    <h2><?php echo $_SESSION['user_name'] ?></h2>
+
+	    <p><strong>Name:</strong>
+	    	<?php echo $_SESSION['first_name'] ." ".$_SESSION['last_name'] ?>
+	    </p>
+	    <p><strong>Email Address:</strong>
+	    	<?php echo $_SESSION['email'] ?>
+	    </p>
+
+	    <form action='Login_register.php' method='post' class='navbar-form navbar-right'>
+        	<input type='submit' value='Log off' class="btn btn-primary btn_left"></div>
+        </form>
     </div>
