edd066e8164681523a281757bf4c22e107fb427f
Theres Scherrer <theresscherrer@gmail.com>
2014-01-15 00:03:30 +0100

    Changed footer placement on page

--- map.php
+++ map.php
@@ -161,6 +161,13 @@
                 <div id="map">
                     <div id="map-canvas"/>
                 </div>
+                <div>
+                    <hr>
+                    <footer>
+                        <p>Made with love by <a href="about.php">Thereza</a>, 2013 | <a href="http://www.linkedin.com/in/thereza">LinkedIn</a> | <a href="https://twitter.com/therezaJS">Twitter</a> | <a href="https://github.com/bakerstreet221b">Github</a></p>
+
+                    </footer><br>
+                </div>
             </div>
         </div>
 
@@ -172,6 +179,13 @@
                 var msnry = new Masonry( container, {
                   itemSelector: '.item'
                 });
+
+                $(window).scroll(function () {
+                   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+                      // alert("Add something at the end of the page");
+                   }
+                });
             });
         </script>
-<?php require('footer.php'); ?>
+    </body>
+</html>
