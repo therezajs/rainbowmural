3dd7068ed0a35ff58e2376a25b923b725cbcff51
Theres Scherrer <theresscherrer@gmail.com>
2014-01-29 14:04:58 -0800

    Fixed indentation

--- header.php
+++ header.php
@@ -2,111 +2,107 @@
 <html>
 <head>
 <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <title>Rainbow Mural</title>
-    <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="rainbowmural_style.css">
-    <link rel="stylesheet" type="text/css" href="dist/css/bootstrap-theme.css">
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYEdp4vZEKpPU4nbucnDEAwzvCgyXCDhQ&amp;sensor=false"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="jquery.js"></script>
-    <script src="http://masonry.desandro.com/masonry.pkgd.js"></script>
-    <script src="dist/js/bootstrap.js"></script>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+  <title>Rainbow Mural</title>
+  <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="rainbowmural_style.css">
+  <link rel="stylesheet" type="text/css" href="dist/css/bootstrap-theme.css">
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCYEdp4vZEKpPU4nbucnDEAwzvCgyXCDhQ&amp;sensor=false"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="jquery.js"></script>
+  <script src="http://masonry.desandro.com/masonry.pkgd.js"></script>
+  <script src="dist/js/bootstrap.js"></script>
 
 </head>
 <body>
-	<nav class="navbar navbar-inverse navbar-fixed-top" id="navbar" role="navigation">
-          <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Rainbow Mural<!--  by</a><a class="navbar-brand" href="http://www.flickr.com"><strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong> --></a>
+  <nav class="navbar navbar-inverse navbar-fixed-top" id="navbar" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">Rainbow Mural<!--  by</a><a class="navbar-brand" href="http://www.flickr.com"><strong style="color:#3993ff">flick<span style="color:#ff1c92">r</span></strong> --></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a></li>
+          <?php if (isset($_SESSION['logged_in'])): ?>
+          <li><a href="like.php">Likes</a></li>
+          <?php endif; ?>
+          <li><a href="about.php">About</a></li>
+        </ul>
+          <form action='Picture.php' method='post' class="navbar-form navbar-right" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control header" placeholder="Choose your city" name="name">
           </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li><a href="index.php">Home</a></li>
-              <?php if (isset($_SESSION['logged_in'])): ?>
+          <input type='hidden' name='action' value='city'>
+          <button type="submit" class="btn btn-success header">Go</button>
+          </form>
+        <ul class="nav navbar-nav navbar-right">
+        <?php
+        if (isset($_SESSION['logged_in'])): ?>
+          <li><a href='edit.php'><?php echo $_SESSION['user_name'] ?></a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <b class="caret"></b></a>
+            <ul class="dropdown-menu">
               <li><a href="like.php">Likes</a></li>
-              <?php endif; ?>
-              <li><a href="about.php">About</a></li>
-            </ul>
-                <form action='Picture.php' method='post' class="navbar-form navbar-right" role="search">
-                <div class="form-group">
-                  <input type="text" class="form-control header" placeholder="Choose your city" name="name">
-                </div>
-                <input type='hidden' name='action' value='city'>
-                <button type="submit" class="btn btn-success header">Go</button>
+              <li><a href="upload.php">Upload pics</a></li>
+              <li><a href='edit.php'>Edit account</a></li>
+              <li class="divider"></li>
+              <li class="li_link">
+                <form action='Login_register.php' method='post' class='navbar-form navbar-left'>
+                <!-- <a href="">Log off</a> -->
+                <input type='submit' class="link" value='Log off'></div>
                 </form>
-
-
-            <ul class="nav navbar-nav navbar-right">
-            <?php
-            if (isset($_SESSION['logged_in'])): ?>
-              <li><a href='edit.php'><?php echo $_SESSION['user_name'] ?></a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="like.php">Likes</a></li>
-                  <li><a href="upload.php">Upload pics</a></li>
-                  <li><a href='edit.php'>Edit account</a></li>
-                  <li class="divider"></li>
-                  <li class="li_link">
-                    <form action='Login_register.php' method='post' class='navbar-form navbar-left'>
-                    <!-- <a href="">Log off</a> -->
-                    <input type='submit' class="link" value='Log off'></div>
-                    </form>
-                  </li>
-                </ul>
               </li>
-              <?php elseif (!isset($_SESSION['logged_in']) && ($_SERVER["REQUEST_URI"] != '/login.php')): ?>
-              <li id='mylogin' class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <b class="caret"></b></a>
-                <ul class="dropdown-menu" id="dropdown">
-                  <form class="" action='Login_register.php' method='post' id='login' role='form'>
-                  <input type='hidden' name='action' value='login'>
-                  <li><input type="text" placeholder="Email" name='email' class="form-control"></li>
-                  <li><input type="password" placeholder="Password" name='password'class="form-control"></li>
-                  <li><button type="submit" class="btn btn-primary">Sign in</button></li>
-                  </form>
-                </ul>
-              </li>
-              <li><a href="login.php">or register</a></li>
-              <?php endif; ?>
             </ul>
-          </div><!-- /.navbar-collapse -->
-        </div>
-    </nav>
+          </li>
+          <?php elseif (!isset($_SESSION['logged_in']) && ($_SERVER["REQUEST_URI"] != '/login.php')): ?>
+          <li id='mylogin' class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <b class="caret"></b></a>
+            <ul class="dropdown-menu" id="dropdown">
+              <form class="" action='Login_register.php' method='post' id='login' role='form'>
+              <input type='hidden' name='action' value='login'>
+              <li><input type="text" placeholder="Email" name='email' class="form-control"></li>
+              <li><input type="password" placeholder="Password" name='password'class="form-control"></li>
+              <li><button type="submit" class="btn btn-primary">Sign in</button></li>
+              </form>
+            </ul>
+          </li>
+          <li><a href="login.php">or register</a></li>
+          <?php endif; ?>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div>
+  </nav>
 
 
 
 <?php
-    function flash(){
+  function flash(){
+    if (isset($_SESSION['errors'])) {
+      foreach ($_SESSION['errors'] as $error) {
+          echo "<div class='alert alert-danger'>".$error."</div>";
+      }
+      unset($_SESSION['errors']);
+    };
 
-        if (isset($_SESSION['errors'])) {
-            foreach ($_SESSION['errors'] as $error) {
-                echo "<div class='alert alert-danger'>".$error."</div>";
-            }
-            unset($_SESSION['errors']);
-        };
-
-        if (isset($_SESSION['messages'])) {
-            foreach ($_SESSION['messages'] as $message) {
-                echo "<div class='alert alert-success'>".$message."</div>";
-            }
-            unset($_SESSION['messages']);
-        };
-
-    }
+    if (isset($_SESSION['messages'])) {
+      foreach ($_SESSION['messages'] as $message) {
+          echo "<div class='alert alert-success'>".$message."</div>";
+      }
+      unset($_SESSION['messages']);
+    };
+  }
 ?>
 
--- login.php
+++ login.php
@@ -1,61 +1,61 @@
 <?php
-	session_start();
-	include("Database.php");
-	require('header.php');
+session_start();
+include("Database.php");
+require('header.php');
 ?>
-	<div class='container' id='my_container'>
-		<?php
-			flash();
-		?>
+<div class='container' id='my_container'>
+  <?php
+    flash();
+  ?>
 
-		<div class='row'>
-			<?php
-            if (isset($_SESSION['logged_in'])): ?>
-				<form action='login_register.php' method='post' role='form'>
-					<div id='submit'>
-					<input type='submit' value='Log off' class='btn btn-danger'>
-					</div>
-				</form>
-			<?php else: ?>
-				<div class='box col-md-6'>
-					<h3>Register</h3>
-					<form action='Login_register.php' method='post' id='registration' role='form'>
-						<input type='hidden' name='action' value='registration'>
-						<div class='form-group'>
-							<input type='text' placeholder='Username' name='user_name' class="form-control">
-						</div>
-						<div class='form-group'>
-							<input type='text' placeholder='First name' name='first_name' class="form-control">
-						</div>
-						<div class='form-group'>
-							<input type='text' placeholder='Last name' name='last_name' class="form-control">
-						</div>
-						<div class='form-group'>
-							<input type='text' placeholder='Email' name='email' class="form-control">
-						</div>
-						<div class='form-group'>
-							<input type='password' placeholder='Password' name='password' class="form-control">
-						</div>
-						<div class='form-group'>
-							<input type='password' placeholder='Confirm Password' name='conf_password' class="form-control">
-						</div>
-						<input type='submit' value='Register' class='btn btn-success'>
-					</form>
-				</div>
-				<div class='box col-md-6'>
+  <div class='row'>
+    <?php
+    if (isset($_SESSION['logged_in'])): ?>
+      <form action='login_register.php' method='post' role='form'>
+        <div id='submit'>
+        <input type='submit' value='Log off' class='btn btn-danger'>
+        </div>
+      </form>
+    <?php else: ?>
+      <div class='box col-md-6'>
+        <h3>Register</h3>
+        <form action='Login_register.php' method='post' id='registration' role='form'>
+          <input type='hidden' name='action' value='registration'>
+          <div class='form-group'>
+            <input type='text' placeholder='Username' name='user_name' class="form-control">
+          </div>
+          <div class='form-group'>
+            <input type='text' placeholder='First name' name='first_name' class="form-control">
+          </div>
+          <div class='form-group'>
+            <input type='text' placeholder='Last name' name='last_name' class="form-control">
+          </div>
+          <div class='form-group'>
+            <input type='text' placeholder='Email' name='email' class="form-control">
+          </div>
+          <div class='form-group'>
+            <input type='password' placeholder='Password' name='password' class="form-control">
+          </div>
+          <div class='form-group'>
+            <input type='password' placeholder='Confirm Password' name='conf_password' class="form-control">
+          </div>
+          <input type='submit' value='Register' class='btn btn-success'>
+        </form>
+      </div>
+      <div class='box col-md-6'>
 
-				<h3>Login</h3>
-					<form action='Login_register.php' method='post' id='login' role='form'>
-						<input type='hidden' name='action' value='login'>
-						<div class='form-group'>
-						<input type='text' placeholder='Email' name='email' class="form-control">
-						</div>
-						<div class='form-group'>
-						<input type='password' placeholder='Password' name='password' class="form-control">
-						</div>
-						<input type='submit' value='Login' class='btn btn-success'>
-					</form>
-				</div>
-			<?php endif; ?>
-		</div>
+      <h3>Login</h3>
+        <form action='Login_register.php' method='post' id='login' role='form'>
+          <input type='hidden' name='action' value='login'>
+          <div class='form-group'>
+          <input type='text' placeholder='Email' name='email' class="form-control">
+          </div>
+          <div class='form-group'>
+          <input type='password' placeholder='Password' name='password' class="form-control">
+          </div>
+          <input type='submit' value='Login' class='btn btn-success'>
+        </form>
+      </div>
+    <?php endif; ?>
+  </div>
 <?php require('footer.php'); ?>
