8008b9fb35c2447ac40a9916abd5bb28c2b35504
Thereza <therezascherrer@gmail.com>
2014-05-26 01:28:59 +0200

    "Formatted like file"

--- application/like.php
+++ application/like.php
@@ -51,43 +51,49 @@ $(document).ready(function(){
   ?>
   <h2>Likes</h2>
   <div class='row' id='like_container'>
-  <?php
-  if (isset($_SESSION['id']))
-  {
+  <?php if (isset($_SESSION['id'])):
     $data = new Fav();
     $likes = $data->getLikes($_SESSION['id']);
-
-    // var_dump($likes);
-    if (!empty($likes))
-    {
-      foreach ($likes as $like) {
-        echo '<div class="item" ><a href="detail.php?lat=' . $like['lat'] . '&lon=' . $like['lon'] . '&id=' . $like['pic_id'] . '&secret=' . $like['pic_secret'] . '"><img src="http://www.flickr.com/photos/'.$like['pic_id'].'_'.$like['pic_secret'].'.jpg"></a><h4>'.$like['title'].'</h4><form action="ajax_like.php" method="post" class="heart_btn">
-          <input type="hidden" name="action" value="like_button">
-          <input type="hidden" name="user_id" value='.$_SESSION['id'].'>
-          <input type="hidden" name="pic_id" value='.$like['pic_id'].'>
-          <input type="hidden" name="pic_secret" value='.$like['pic_secret'].'>
-          <input type="hidden" name="lon" value='. $like['lon'] .'>
-          <input type="hidden" name="lat" value='. $like['lat'] .'>
-          <input type="hidden" name="name" value='. $like['title'] .'>
-          <input type="hidden" name="like_location" value="undefined">
-          <button class="heart_span"><span class="glyphicon glyphicon-heart"></span></button></form>
-          <form action="ajax_like.php" method="post" class="check_heart_button">
-          <input type="hidden" name="action" value="check_like_button">
-          <input type="hidden" name="user_id" value='.$_SESSION['id'].'>
-          <input type="hidden" name="pic_id" value='.$like['pic_id'].'></form></div>';
-      }
-    }
-    else
-    {
-      echo "<h3> No likes set yet</h3>";
-    };
-  }
-  else
-  {
-    echo "<h3>Please log in to see your likes</h3>";
-  };
-
   ?>
+    <?php if (!empty($likes)): ?>
+      <?php foreach ($likes as $like):
+        $lat = $like['lat'];
+        $lon = $like['lon'];
+        $id = $like['pic_id'];
+        $secret = $like['pic_secret'];
+        $title = $like['title'];
+      ?>
+        <div class="item" >
+          <a href="detail.php?lat=<?php echo $lat ?>&lon=<?php echo $lon ?>&id=<?php echo $id ?>&secret=<?php echo $secret ?>">
+            <img src="http://www.flickr.com/photos/<?php echo $id ?>_<?php echo $secret ?>.jpg">
+          </a>
+          <h4><?php echo $title ?></h4>
+          <form action="ajax_like.php" method="post" class="heart_btn">
+            <input type="hidden" name="action" value="like_button">
+            <input type="hidden" name="user_id" value='<?php echo $_SESSION['id'] ?>'>
+            <input type="hidden" name="pic_id" value='<?php echo $id ?>'>
+            <input type="hidden" name="pic_secret" value='<?php echo $secret ?>'>
+            <input type="hidden" name="lon" value='<?php echo $lon ?>'>
+            <input type="hidden" name="lat" value='<?php echo $lat ?>'>
+            <input type="hidden" name="name" value='<?php echo $title ?>'>
+            <input type="hidden" name="like_location" value="undefined">
+            <button class="heart_span">
+              <span class="glyphicon glyphicon-heart"></span>
+            </button>
+          </form>
+          <form action="ajax_like.php" method="post" class="check_heart_button">
+            <input type="hidden" name="action" value="check_like_button">
+            <input type="hidden" name="user_id" value='<?php echo $_SESSION['id'] ?>'>
+            <input type="hidden" name="pic_id" value='<?php echo $id ?>'>
+          </form>
+        </div>
+      <?php endforeach; ?>
+    <?php else: ?>
+      <h3> No likes set yet</h3>
+    <?php endif; ?>
+  <?php else: ?>
+    <h3>Please log in to see your likes</h3>
+  <?php endif; ?>
   </div>
   <script type="text/javascript">
     $(window).load(function() {
@@ -95,7 +101,6 @@ $(document).ready(function(){
       var msnry = new Masonry( container, {
         itemSelector: '.item'
       });
-
     });
   </script>
 </div>
