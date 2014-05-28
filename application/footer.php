2095a77928d0d44ac4875102edf0b23df33bae99
Thereza <therezascherrer@gmail.com>
2014-05-28 12:02:27 +0200

    Formatting changes

--- application/footer.php
+++ application/footer.php
@@ -1,8 +1,12 @@
   <div class="container">
     <hr>
     <footer>
-      <p>Made with love by <a href="about.php">Thereza</a>, 2013 | <a href="http://www.linkedin.com/in/thereza">LinkedIn</a> | <a href="https://twitter.com/therezaJS">Twitter</a> | <a href="https://github.com/therezajs">Github</a></p>
-
+      <p>Made with love by
+      	<a href="about.php">Thereza</a>, 2013 |
+      	<a href="http://www.linkedin.com/in/thereza">LinkedIn</a> |
+      	<a href="https://twitter.com/therezaJS">Twitter</a> |
+      	<a href="https://github.com/therezajs">Github</a>
+      </p>
     </footer><br>
   </div>
 </body>
